29e5536413a1e5f35d83c7554e46637138dfd9b0
Michael Constanza <michael.constanza@gmail.com>
2016-10-06 20:06:37 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,48 @@
+var keys = require('keys.js');
+
+// node liri.js my-tweets
+// This will show your last 20 tweets and when they were created at in your terminal/bash window.
+function my-tweets(){
+
+};
+
+// node liri.js spotify-this-song '<song name here>'
+// This will show the following information about the song in your terminal/bash window
+		//* Artist(s)
+		//* The song's name
+		//* A preview link of the song from Spotify
+		//* The album that the song is from
+
+//if no song is provided then your program will default to "The Sign" by Ace of Base
+
+function spotify-this-song() {
+
+};
+
+// node liri.js movie-this '<movie name here>
+// This will output the following information to your terminal/bash window:
+
+	// 	* Title of the movie.
+	// 	* Year the movie came out.
+	// 	* IMDB Rating of the movie.
+	// 	* Country where the movie was produced.
+	// 	* Language of the movie.
+	// 	* Plot of the movie.
+	// 	* Actors in the movie.
+	// 	* Rotten Tomatoes Rating.
+	// 	* Rotten Tomatoes URL.
+
+	// * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+	// 	* If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+	// 	* It's on Netflix!
+function movie-this() {
+
+};	
+
+// node liri.js do-what-it-says`
+	// * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+	// 	* It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+	// 	* Feel free to change the text in that document to test out the feature for other commands.
+function do-what-it-says() {
+
+};	
